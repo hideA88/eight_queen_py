90852d82c3df6e4cfc145bf102bda04ea56482bb
hideaki mac <hideakit803@gmail.com>
2012-03-24 18:02:18 +0900

    置けない場所をセットするメソッドがおかしい

--- eihgt_queen.py
+++ eihgt_queen.py
@@ -1,4 +1,4 @@
-
+from copy import deepcopy
 NONE=0
 PUT_QUEEN=1
 CANT_PUT=2
@@ -7,44 +7,44 @@ def getBoard(defaultVal, size):
     col = [defaultVal for j in range(size)] 
     board = [col for i in range(size)]
     return board
+    
+def showboard(board):
+    print 
+    for row in board:
+        print row
+    print 
 
 def setQueen(board, putNum):
     size = len(board)
-    if putNum == size - 1:
-        return
+    print putNum
+    if  size - 1 < putNum:
+        return board
     col = board[putNum]
     index = 0
     boards=[]
     for val in col:
         if val == NONE:
-            restBoard = board[putNum + 1 : size ]
-            checkCantPutLine(index, restBoard)
-            restBoard = checkCantPutSkew(index,restBoard)
-            # check cant ph
-            # call setQueen method
-            boards.append()
+            newBoard = deepcopy(board)
+            newBoard[putNum][index] = PUT_QUEEN
+            newBoard = checkCantPut(putNum + 1, index, newBoard, 0, lambda setIndex, size : True)
+            newBoard = checkCantPut(putNum + 1, index, newBoard, 1, lambda setIndex, size : setIndex < size)
+            newBoard = checkCantPut(putNum + 1, index, newBoard, -1, lambda setIndex, size : -1 < setIndex)
+            #if putNum == 0 :
+                #showboard(newBoard)
+            boards.append(setQueen(newBoard, putNum + 1))
         index = index + 1 
     return boards
             
-def checkCantPutSkew(colIndex, index, restBoard, thread, condition):
-    if len(restBoard) < colIndex :
-        return restBoard
-    col = restBoard[colIndex]
-    if condition():
-        col[index + thread] = CANT_PUT
-        return checkCantPutSkew(colIndex+1, index, restBoard, thread , condition)
-    return restBoard
+def checkCantPut(colIndex, preRowIndex, board, thread, condition):
+    if len(board) - 1 < colIndex :
+        return board
+    index= preRowIndex + thread
+    if condition(index, len(board)):
+        board[colIndex][index] = CANT_PUT
+        return checkCantPut(colIndex+1, index, board, thread , condition)
+    return board
 
-def checkCantPutLine(index, restBoard):
-    for col in restBoard:
-        col[index]=CANT_PUT
-    return restBoard
     
-def showBoard(board):
-    for row in board:
-        print row
-
-board = getBoard(NONE,4)
-showBoard(board)
-#print setQueen(board, 0)
+board = getBoard(NONE,8)
+setQueen(board, 0)
 
