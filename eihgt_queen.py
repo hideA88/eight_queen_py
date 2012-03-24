532ddee7308ee32932b521d2a87b3adb7f35c08c
hideaki mac <hideakit803@gmail.com>
2012-03-24 16:11:19 +0900

    大体できたかも

--- eihgt_queen.py
+++ eihgt_queen.py
@@ -1,13 +1,50 @@
-def getBoard(size):   
-    row=[1 for j in range(size)] 
-    board = [row for i in range(size)]
+
+NONE=0
+PUT_QUEEN=1
+CANT_PUT=2
+
+def getBoard(defaultVal, size):
+    col = [defaultVal for j in range(size)] 
+    board = [col for i in range(size)]
     return board
 
-print getBoard(4)
-
-def check(col,num,size):
-    if num==size:
+def setQueen(board, putNum):
+    size = len(board)
+    if putNum == size - 1:
         return
-    nextCol=range(size)
+    col = board[putNum]
+    index = 0
+    boards=[]
+    for val in col:
+        if val == NONE:
+            restBoard = board[putNum + 1 : size ]
+            checkCantPutLine(index, restBoard)
+            restBoard = checkCantPutSkew(index,restBoard)
+            # check cant ph
+            # call setQueen method
+            boards.append()
+        index = index + 1 
+    return boards
+            
+def checkCantPutSkew(colIndex, index, restBoard, thread, condition):
+    if len(restBoard) < colIndex :
+        return restBoard
+    col = restBoard[colIndex]
+    if condition():
+        col[index + thread] = CANT_PUT
+        return checkCantPutSkew(colIndex+1, index, restBoard, thread , condition)
+    return restBoard
 
+def checkCantPutLine(index, restBoard):
+    for col in restBoard:
+        col[index]=CANT_PUT
+    return restBoard
     
+def showBoard(board):
+    for row in board:
+        print row
+
+board = getBoard(NONE,4)
+showBoard(board)
+#print setQueen(board, 0)
+
